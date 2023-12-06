6ba9afde7cd3bc941472744cb3ed3658724c5b17
JulianAmado <j.amadosena@gmail.com>
2023-12-06 16:43:47 -0500

    Index.php

--- public/index.php
+++ public/index.php
@@ -1,55 +1,245 @@
-<?php
+<!doctype html>
+<html lang="en">
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>BuilDoc</title>
+  <link rel="shortcut icon" href="..\resources\img\HeadLogo.png"
+    type="image/x-icon">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="..\resources\css\app.css">
+  <iframe src="..\resources\html\Header.html" width="100%" height="100%"></iframe>
+</head>
 
-define('LARAVEL_START', microtime(true));
+<body>
+  <div class="container">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg">
+          <h1 class="text-center display-4 fw-normal text-body-emphasis">¡Bienvenido a la era de la gestión eficiente
+            con
+            BuilDoc!</h1>
+        </div>
+      </div>
+      <div class="container text-center">
+        <hr width="me-lg-auto">
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg">
+            <img class="Imagen" src="..\resources\img\PaginaPrincipal.jpg" alt="Imagen" width="100%" height="100%">
+          </div>
+        </div>
+      </div>
+      <div class="container text-center">
+        <hr width="me-lg-auto">
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg">
+            <h2 id="DescripcionId" class="display-4 fw-normal text-body-emphasis text-center">Descripcion General</h2>
+            <p id="Descripcion">BuilDoc es la solución que tu empresa necesita para simplificar la gestión de archivos,
+              tareas, incidentes e inspecciones. Descubre cómo esta aplicación puede transformar la forma en que operas
+              y
+              cómo puedes lograr más con menos esfuerzo. BuilDoc es una innovadora aplicación web diseñada para brindar
+              una solución integral de gestión para tu empresa. Esta potente herramienta está diseñada para simplificar
+              y
+              optimizar la administración de archivos, tareas, incidentes e inspecciones, permitiéndote mejorar la
+              eficiencia y la productividad en todos los aspectos de tu organización.</p>
+          </div>
+        </div>
+      </div>
+    </div>
 
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
+    <div class="container text-center">
+      <hr width="me-lg-auto">
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg">
+          <h2 id="FuncionesId" class="display-4 fw-normal text-body-emphasis text-center">Funciones</h2>
+        </div>
+      </div>
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 text-center">
+            <h2 id="Titulos">Gestion de Archivos</h2>
+            <p>En la aplicación BuilDoc, puedes organizar, cargar, renombrar, mover y compartir archivos. También se
+              pueden crear carpetas, proyectos y asignar clientes. Esto mejora la gestión de documentos y proyectos,
+              impulsando la productividad.</p>
+            <img class="align-items-center" src="..\resources\img\GA.png" alt="" width="50%" height="50%">
+          </div>
+          <div class="col-lg-4 text-center">
+            <h2 class="text-center">Gestion de Tareas</h2>
+            <p>El módulo de tareas de BuilDoc permite administrar fases, crear, modificar, eliminar, asignar, aprobar,
+              comentar y adjuntar archivos a tareas. Facilita la colaboración y mejora la eficiencia en el seguimiento
+              del
+              progreso del proyecto.</p>
+            <img class="align-items-center" src="..\resources\img\GT.png" alt="" width="50%" height="50%">
+          </div>
+          <div class="col-lg-4 text-center">
+            <h2 class="text-center">Gestion de Calidad</h2>
+            <p>La gestión de incidentes e inspecciones en BUILDOC es esencial en construcción. Registra incidentes,
+              programa inspecciones y mejora la calidad y seguridad. Informes facilitan decisiones y la mejora continua
+              en
+              la industria.</p>
+            <img class="align-items-center" src="..\resources\img\GC.png" alt="" width="50%" height="50%">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container text-center">
+      <hr width="me-lg-auto">
+    </div>
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
+    <div class="container">
+      <header>
+        <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+          <h1 id="PreciosId" class="display-4 fw-normal text-body-emphasis">Precios</h1>
+          <p class="fs-5 text-body-secondary">Elije el plan que mejor se adapte a tus necesidades.
+          </p>
+        </div>
+      </header>
 
-require __DIR__.'/../vendor/autoload.php';
+      <div class="container">
+        <main>
+          <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+            <div class="col-lg-4">
+              <div class="card mb-4 rounded-3 shadow-sm">
+                <div class="card-header py-3">
+                  <h4 class="my-0 fw-normal">Plan Básico</h4>
+                </div>
+                <div class="card-body">
+                  <h1 class="card-title pricing-card-title">COP 60.000<small
+                      class="text-body-secondary fw-light">/mes</small>
+                  </h1>
+                  <ul class="list-unstyled mt-3 mb-4">
+                    <li>5 GB de almacenamiento</li>
+                    <li>Soporte por correo electrónico</li>
+                    <li>Acceso a plantillas premium</li>
+                  </ul>
+                  <button type="button" class="w-100 btn btn-lg btn-dark"
+                    style="background-color: #0074e4;">Elegir</button>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="card mb-4 rounded-3 shadow-sm">
+                <div class="card-header py-3">
+                  <h4 class="my-0 fw-normal">Plan Estándar</h4>
+                </div>
+                <div class="card-body">
+                  <h1 class="card-title pricing-card-title">COP 90.000<small
+                      class="text-body-secondary fw-light">/mes</small>
+                  </h1>
+                  <ul class="list-unstyled mt-3 mb-4">
+                    <li>15 GB de almacenamiento</li>
+                    <li>Soporte por correo y teléfono</li>
+                    <li>Acceso a plantillas premium</li>
+                  </ul>
+                  <button type="button" class="w-100 btn btn-lg btn-dark"
+                    style="background-color: #0074e4;">Elegir</button>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="card mb-4 rounded-3 shadow-sm">
+                <div class="card-header py-3">
+                  <h4 class="my-0 fw-normal">Plan Premium</h4>
+                </div>
+                <div class="card-body">
+                  <h1 class="card-title pricing-card-title">COP 120.000<small
+                      class="text-body-secondary fw-light">/mes</small>
+                  </h1>
+                  <ul class="list-unstyled mt-3 mb-4">
+                    <li>50 GB de almacenamiento</li>
+                    <li>Soporte prioritario 24/7</li>
+                    <li>Acceso a plantillas premium</li>
+                  </ul>
+                  <button type="button" class="Color w-100 btn btn-lg btn-dark"
+                    style="background-color: #0074e4;">Contactenos</button>
+                </div>
+              </div>
+            </div>
+          </div>
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
+      </div>
+      </main>
+    </div>
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+    <div class="container text-center">
+      <hr width="me-lg-auto">
+    </div>
 
-$kernel = $app->make(Kernel::class);
+    <div class="row">
+      <div class="col-lg">
+        <h1 id="ClientesId" class="display-4 fw-normal text-body-emphasis text-center">Clientes</h1>
+      </div>
+    </div>
 
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
+    <div class="container">
+      <div class="row">
+        <div class="col-lg">
+          <div id="carouselExampleCaptions" class="carousel slide">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="..\resources\img\Carrusel2.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Constructora Bolivar</h5>
+                  <p>Constructora Bolivar es una empresa líder en la construcción de viviendas y urbanizaciones en
+                    Colombia.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="..\resources\img\Carrusel.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Grupo Argos</h5>
+                  <p>Grupo Argos es una empresa diversificada que opera en la industria del cemento y la infraestructura
+                    en Colombia.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="..\resources\img\Carrusel3.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Triada S.A.</h5>
+                  <p>Triada S.A. es una empresa especializada en proyectos de viviendas y edificios comerciales en
+                    Colombia.</p>
+                </div>
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+              data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+              data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
 
-$kernel->terminate($request, $response);
+  </div>
+  <iframe src="..\resources\html\Footer.html" width="100%" height="227px"></iframe>
+  </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
